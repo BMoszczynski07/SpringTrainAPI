bea6e451d311dfe58de42a921335f558181204a5
Bartosz Moszczyński <109615293+BMoszczynski07@users.noreply.github.com>
2023-10-13 09:57:24 +0200

    initial commit

---
+++ src/main/java/com/example/trainapi/TrainApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainApiApplication.class, args);
+    }
+
+}
